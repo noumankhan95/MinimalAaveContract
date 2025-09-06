18847b91b60683bdcf93cfc992a1a60db2b90e97
nouman <noumankhan95@yahoo.com>
2025-09-06 16:21:58 +0500

    added scripts for deployment

---
+++ script/Deploy.s.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {DefiCoin} from "../src/DefiCoin.sol";
+import {Market} from "../src/Market.sol";
+
+contract Deploy is Script {
+    Market market;
+    HelperConfig.NetworkConfig config;
+    HelperConfig hconfig;
+    address[] public priceFeeds;
+    address[] public tokenaddresses;
+
+    function setUp() public {
+        hconfig = new HelperConfig();
+        config = hconfig.getChainConfig(block.chainid);
+        DefiCoin defiCoin = new DefiCoin();
+        priceFeeds = [config.WethpriceFeed, config.WbtcpriceFeed];
+        tokenaddresses = [config.WethAddress, config.WbtcAddress];
+        market = new Market(priceFeeds, tokenaddresses, defiCoin);
+        defiCoin.transferOwnership(address(market));
+    }
+}
--- src/DefiCoin.sol
+++ src/DefiCoin.sol
@@ -3,15 +3,16 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract DefiCoin is ERC20 {
-    constructor() ERC20("Defi Coin", "DFI") {}
+contract DefiCoin is ERC20, Ownable {
+    constructor() ERC20("Defi Coin", "DFI") Ownable(msg.sender) {}
 
-    function mint(address _account, uint256 _amount) public {
+    function mint(address _account, uint256 _amount) public onlyOwner {
         _mint(_account, _amount);
     }
 
-    function burn(address _account, uint256 _amount) public {
+    function burn(address _account, uint256 _amount) public onlyOwner {
         _burn(_account, _amount);
     }
 }
